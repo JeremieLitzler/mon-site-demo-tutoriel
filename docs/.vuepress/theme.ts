3d29bb834cd31b469f252a1a3a5549ba98e66604
Jeremie Litzler <jeremie.litzler@gmail.com>
2023-11-06 20:22:17 +0100

    bump theme to beta.245

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -1,30 +1,30 @@
 //https://v2.vuepress.vuejs.org/reference/default-theme/extending.html
 
-import { hopeTheme } from 'vuepress-theme-hope';
+import { hopeTheme } from "vuepress-theme-hope";
 
-import { enNavBarConfig, frNavBarConfig } from './configs/navbar';
+import { enNavBarConfig, frNavBarConfig } from "./configs/navbar";
 
 export default hopeTheme({
   //path.resolve(__dirname, './theme'),
-  hostname: 'https://mon-site-demo-tutoriel.madebyjeremie.fr',
+  hostname: "https://mon-site-demo-tutoriel.madebyjeremie.fr",
   author: {
-    name: 'Jeremie Litzler',
-    url: 'https://mon-site-demo-tutoriel.madebyjeremie.fr',
+    name: "Jeremie Litzler",
+    url: "https://mon-site-demo-tutoriel.madebyjeremie.fr",
   },
-  repo: 'JeremieLitzler/mon-site-demo-tutoriel',
-  docsRepo: 'https://github.com/JeremieLitzler/mon-site-demo-tutoriel',
-  docsBranch: 'main',
-  docsDir: 'docs',
-  editLinkPattern: ':repo/edit/:branch/:path',
-  logo: '/icons/favicon-32x32.png',
-  logoDark: '/icons/dark-favicon-32x32.png',
-  darkmode: 'enable',
+  repo: "JeremieLitzler/mon-site-demo-tutoriel",
+  docsRepo: "https://github.com/JeremieLitzler/mon-site-demo-tutoriel",
+  docsBranch: "main",
+  docsDir: "docs",
+  editLinkPattern: ":repo/edit/:branch/:path",
+  logo: "/icons/favicon-32x32.png",
+  logoDark: "/icons/dark-favicon-32x32.png",
+  darkmode: "enable",
   locales: {
-    '/': {
+    "/": {
       navbar: frNavBarConfig,
       sidebar: false,
     },
-    '/en/': {
+    "/en/": {
       navbar: enNavBarConfig,
       sidebar: false,
     },
@@ -43,7 +43,7 @@ export default hopeTheme({
     //   mapping: 'title',
     // },
     mdEnhance: {
-      linksCheck: 'always',
+      linksCheck: "always",
       //See https://vuepress-theme-hope.github.io/v2/guide/markdown/align.html
       align: true,
       //See https://vuepress-theme-hope.github.io/v2/guide/markdown/attrs.html
@@ -53,7 +53,8 @@ export default hopeTheme({
       //See https://vuepress-theme-hope.github.io/v2/guide/markdown/container.html
       container: true,
       //See https://vuepress-theme-hope.github.io/v2/guide/markdown/echarts.html
-      echarts: true,
+      //Require to install echarts package
+      //echarts: true,
       //See https://vuepress-theme-hope.github.io/v2/guide/markdown/footnote.html
       //See also https://echarts.apache.org/en/index.html
       footnote: true,
