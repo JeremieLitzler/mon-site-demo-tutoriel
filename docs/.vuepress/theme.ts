8409129302eb6670a09f42b7819c5dc12efd174f
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-02-23 07:04:11 +0100

    bump to theme rc.26

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -43,7 +43,7 @@ export default hopeTheme({
     //   mapping: 'title',
     // },
     mdEnhance: {
-      linksCheck: "always",
+      // linksCheck: "always",
       //See https://vuepress-theme-hope.github.io/v2/guide/markdown/align.html
       align: true,
       //See https://vuepress-theme-hope.github.io/v2/guide/markdown/attrs.html
